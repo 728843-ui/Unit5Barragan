f93828aae1c840392a77aff543645f1c60b28b22
728843-ui <728843@student.dusd.net>
2026-05-15 15:26:50 -0700

    fgsdfhb
    nyhfgfgn fdgb x

---
+++ My personal project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zBound = 6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    //Prevent the player from leaving from the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}
