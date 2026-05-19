3862165943c61967d6a06ca075552d676b8702fc
728843-ui <728843@student.dusd.net>
2026-05-19 15:22:40 -0700

    zx zxc zx zxv xz

--- BarraganUnit5/Assets/Course Library/_Source_Files/Scripts/Target.cs
+++ BarraganUnit5/Assets/Course Library/_Source_Files/Scripts/Target.cs
@@ -7,7 +7,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,18 +27,29 @@ public class Target : MonoBehaviour
         
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    
     Vector3 RandomForce()
     {
-        return Vector3.up * Random.Range(minSpeed,maxSpeed);
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
     float RandomTorque()
     {
         return Random.Range(-maxTorque, maxTorque);
     }
-
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
+  
 }
